3e6a8e19a82425849bbc25c6e022ff115d51def6
“praveenkavedi” <kvpraveen14@gmail.com>
2024-03-11 12:52:44 +0530

    removed keys

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIAXYKJSZ2JAI6D2FP7"
-    secret_key = "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"
+    #access_key = "AKIAXYKJSZ2JAI6D2FP7"
+    #secret_key = "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"
 }
 resource "aws_instance" "myvm" {
     ami = "ami-0ba259e664698cbfc"
